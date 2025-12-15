57470ecec64592566377039e1cd8bb822d037a8c
ShaikhArhan <arhanshaikh9334@gmail.com>
2025-12-15 18:32:48 +0530

    error fix

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const PORT = process.env.PORT;
 // Middleware to parse JSON
 app.use(express.json());
 
-// Serve API Documentation UI (static files from "API UI" folder)
-app.use(express.static(path.join(__dirname, "API UI")));
+// Serve API Documentation UI (static files from "api_ui" folder)
+app.use(express.static(path.join(__dirname, "api_ui")));
 
 // Serve index.html at root
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "API UI", "index.html"));
+  res.sendFile(path.join(__dirname, "api_ui", "index.html"));
 });
 
 // Use routes
